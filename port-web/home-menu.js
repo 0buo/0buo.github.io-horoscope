c436130109e12a1cc316f113c38f023aee81d676
0buo <64718986+0buo@users.noreply.github.com>
2021-02-21 04:01:09 -0800

    adj

--- port-web/home-menu.js
+++ port-web/home-menu.js
@@ -128,7 +128,7 @@ function swipedetect(el, callback){
     distX,
     distY,
     threshold = 150, //required min distance traveled to be considered swipe
-    restraint = 100, // maximum distance allowed at the same time in perpendicular direction
+    //restraint = 100, // maximum distance allowed at the same time in perpendicular direction
     allowedTime = 300, // maximum time allowed to travel that distance
     elapsedTime,
     startTime,
@@ -154,12 +154,13 @@ function swipedetect(el, callback){
         distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
         elapsedTime = new Date().getTime() - startTime // get time elapsed
         if (elapsedTime <= allowedTime){ // first condition for awipe met
-            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
+                                            //&& Math.abs(distY) <= restraint
+            if (Math.abs(distX) >= threshold                                  ){ // 2nd condition for horizontal swipe met
                 swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
             }
-            else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
-                swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
-            }
+            // else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
+            //     swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+            // }
         }
         handleswipe(swipedir)
         if(el !== window) e.preventDefault()
