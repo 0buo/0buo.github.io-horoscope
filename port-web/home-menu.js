23dd978dc8aee1ea15001aa82db74d5128b786b3
0buo <64718986+0buo@users.noreply.github.com>
2021-02-22 07:10:15 -0800

    1

--- port-web/home-menu.js
+++ port-web/home-menu.js
@@ -527,11 +527,11 @@ class Menus{
 let isMobileTablet = mobileAndTabletCheck();
 //if mobile change html svg displacement attribute sacle
 if(isMobileTablet){
-    document.getElementById(`displacement0`).setAttribute(`scale`, `30`);
-    document.getElementById(`displacement1`).setAttribute(`scale`, `39`);
-    document.getElementById(`displacement2`).setAttribute(`scale`, `24`);
-    document.getElementById(`displacement3`).setAttribute(`scale`, `30`);
-    document.getElementById(`displacement4`).setAttribute(`scale`, `21`);
+    document.getElementById(`displacement0`).setAttribute(`scale`, `27`);
+    document.getElementById(`displacement1`).setAttribute(`scale`, `36`);
+    document.getElementById(`displacement2`).setAttribute(`scale`, `21`);
+    document.getElementById(`displacement3`).setAttribute(`scale`, `27`);
+    document.getElementById(`displacement4`).setAttribute(`scale`, `18`);
 }
 menus = new Menus();
 
