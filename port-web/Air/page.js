32cab0f230a7146fa51e8dc6a7e6b87e979ecf6d
0buo <64718986+0buo@users.noreply.github.com>
2021-02-26 11:52:20 -0800

    1

--- port-web/Air/page.js
+++ port-web/Air/page.js
@@ -462,7 +462,7 @@ function buttonEvents(){
 function scrollHorizontal(){
     flexContainer.addEventListener(`wheel`, function(e){
         let delta = Math.abs(e.deltaY) > Math.abs(e.deltaX) ? e.deltaY : e.deltaX;
-        scrollDeltaH += delta * 0.05;
+        scrollDeltaH += delta * 0.03;
         e.preventDefault();
 
         cancelAnimationFrame(IDscroll);
