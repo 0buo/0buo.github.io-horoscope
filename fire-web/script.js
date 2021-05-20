e12371a111846981fb17676ef412ce4c4d01a64b
0buo <64718986+0buo@users.noreply.github.com>
2021-05-20 11:20:24 -0700

    1

--- fire-web/script.js
+++ fire-web/script.js
@@ -2,6 +2,7 @@ function main(){
     let centers = document.getElementsByClassName(`center`);
 
     let demoVid = document.getElementById(`demoVid`);
+    let lastScrollTop = document.documentElement.scrollTop;
 
     function onResize(){
         for(let i = 0; i < centers.length; i++){
@@ -11,10 +12,18 @@ function main(){
     }
 
     function autoPlay(){
-        if (document.documentElement.scrollTop >= demoVid.offsetTop - window.innerHeight) {
-            demoVid.muted = true;
-            demoVid.play();
+        if (document.documentElement.scrollTop > lastScrollTop){
+            if (document.documentElement.scrollTop >= demoVid.offsetTop - window.innerHeight) {
+                demoVid.muted = true;
+                demoVid.play();
+            }
         }
+        else{
+            if (document.documentElement.scrollTop < demoVid.offsetTop - window.innerHeight) {
+                demoVid.pause();
+            }
+        }
+        lastScrollTop = document.documentElement.scrollTop
     }
 
     onResize();
