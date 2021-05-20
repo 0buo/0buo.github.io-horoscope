fe88d3a1a5dcf65fffeeea44d4c99eec6e08ec81
0buo <64718986+0buo@users.noreply.github.com>
2021-05-20 07:09:12 -0700

    1

--- fire-web/script.js
+++ fire-web/script.js
@@ -1,14 +1,14 @@
 function main(){
     let centers = document.getElementsByClassName(`center`);
 
-    let photo = document.getElementById(`photo`);
+    let shuoming = document.getElementById(`shuoming-image`);
 
     function onResize(){
         for(let i = 0; i < centers.length; i++){
             let margin = (window.innerWidth - centers[i].offsetWidth) / 2;
             centers[i].style.setProperty(`margin-left`, `${margin}px`);
             console.log(margin)
-        }
+        }       
     }
 
     onResize();
