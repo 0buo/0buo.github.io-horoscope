995a3fc1ad3054b6b327ff9d3a6ad94cd010373c
0buo <zhuang0buo@outlook.com>
2021-11-11 04:18:51 -0800

    1

--- gtl-header-test/index.js
+++ gtl-header-test/index.js
@@ -1,6 +1,35 @@
+const lottieDiv = document.querySelector(`.header-lottie`);
+//set lottie div size responsively
+const setLottieDivSize = () => {
+    if (window.innerWidth >= window.innerHeight) {
+        if (window.innerHeight > 430) {
+            lottieDiv.style.setProperty(`height`, `100vh`);
+            lottieDiv.style.setProperty(`width`, `auto`);
+        }
+        else {
+            lottieDiv.style.setProperty(`width`, `50vw`);
+            lottieDiv.style.setProperty(`height`, `auto`);
+        }
+    }
+    else {
+        lottieDiv.style.setProperty(`height`, `auto`);
+        lottieDiv.style.setProperty(`width`, `100vw`);
+    }
+};
+setLottieDivSize();
+window.addEventListener(`resize`, setLottieDivSize);
+
+//set header background height responsively
+const setHeaderPeopleHeight = () => {
+    const headerBg = document.querySelector(`.header-bg`);
+    const titleFlexHeight = parseFloat(getComputedStyle(document.querySelector(`.title-container`)).getPropertyValue(`height`));
+    console.log(titleFlexHeight);
+    headerBg.style.setProperty(`height`, `${titleFlexHeight}px`);
+}
+
+
 //LOTTIE
 //Lottie web doc: https://airbnb.io/lottie/#/web
-const lottieDiv = document.querySelector(`.header-lottie`);
 const playHeaderAnim = () => {
     const headerAnim = bodymovin.loadAnimation({
         container: lottieDiv,
@@ -14,15 +43,18 @@ const playHeaderAnim = () => {
     //opening animation
     headerAnim.addEventListener(`DOMLoaded`, function () {
         headerAnim.playSegments([0, 320], true);
-        
+
         //looping animation
         function loopHeaderAnim() {
             headerAnim.playSegments([321, 1200], true);
         }
         headerAnim.addEventListener(`complete`, loopHeaderAnim);
+
+        //set height of header background after lottie is loaded
+        setHeaderPeopleHeight();
+        window.addEventListener(`resize`, setHeaderPeopleHeight);
     })
 }
-
 
 //PAN PEOPLE
 const panHeaderPeople = () => {
@@ -32,26 +64,6 @@ const panHeaderPeople = () => {
 
     requestAnimationFrame(panHeaderPeople);
 }
-
-//set lottie div size responsively
-const setLottieDivSize = () => {
-    if (window.innerWidth >= window.innerHeight) {
-        if(window.innerHeight > 430){
-            lottieDiv.style.setProperty(`height`, `100vh`);
-            lottieDiv.style.setProperty(`width`, `auto`);
-        }
-        else{
-            lottieDiv.style.setProperty(`width`, `50vw`);
-            lottieDiv.style.setProperty(`height`, `auto`);
-        }
-    }
-    else {
-            lottieDiv.style.setProperty(`height`, `auto`);
-            lottieDiv.style.setProperty(`width`, `100vw`);
-    }
-};
-setLottieDivSize();
-window.addEventListener(`resize`, setLottieDivSize);
 
 requestAnimationFrame(panHeaderPeople);
 playHeaderAnim();
