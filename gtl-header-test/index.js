726f82f8c5c4806a1139fbd333b911799da45df0
0buo <zhuang0buo@outlook.com>
2021-11-15 17:37:28 -0800

    1

--- gtl-header-test/index.js
+++ gtl-header-test/index.js
@@ -1,3 +1,49 @@
+/**
+ * Behaves the same as setTimeout except uses requestAnimationFrame() where possible for better performance
+ * @param {function} fn The callback function
+ * @param {int} delay The delay in milliseconds
+ */
+
+ window.requestTimeout = function(fn, delay) {
+	if( !window.requestAnimationFrame      	&& 
+		!window.webkitRequestAnimationFrame && 
+		!(window.mozRequestAnimationFrame && window.mozCancelRequestAnimationFrame) && // Firefox 5 ships without cancel support
+		!window.oRequestAnimationFrame      && 
+		!window.msRequestAnimationFrame)
+			return window.setTimeout(fn, delay);
+			
+	var start = new Date().getTime(),
+		handle = new Object();
+		
+	function loop(){
+		var current = new Date().getTime(),
+			delta = current - start;
+			
+		delta >= delay ? fn.call() : handle.value = requestAnimationFrame(loop);
+	};
+	
+	handle.value = requestAnimationFrame(loop);
+	return handle;
+};
+
+/**
+ * Behaves the same as clearTimeout except uses cancelRequestAnimationFrame() where possible for better performance
+ * @param {int|object} fn The callback function
+ */
+window.clearRequestTimeout = function(handle) {
+  if(handle === undefined){handle = {value: undefined};}
+  window.cancelAnimationFrame ? window.cancelAnimationFrame(handle.value) :
+  window.webkitCancelAnimationFrame ? window.webkitCancelAnimationFrame(handle.value) :
+  window.webkitCancelRequestAnimationFrame ? window.webkitCancelRequestAnimationFrame(handle.value) : /* Support for legacy API */
+  window.mozCancelRequestAnimationFrame ? window.mozCancelRequestAnimationFrame(handle.value) :
+  window.oCancelRequestAnimationFrame	? window.oCancelRequestAnimationFrame(handle.value) :
+  window.msCancelRequestAnimationFrame ? window.msCancelRequestAnimationFrame(handle.value) :
+  clearTimeout(handle);
+};
+
+
+//======================================================================
+//main header functions
 const lottieDiv = document.querySelector(`.header-lottie`);
 //set lottie div size responsively
 const setLottieDivSize = () => {
@@ -80,4 +126,4 @@ const titleTransition = () => {
 
 requestAnimationFrame(panHeaderPeople);
 playHeaderAnim();
-setTimeout(titleTransition, 500);
+requestTimeout(titleTransition, 500);
